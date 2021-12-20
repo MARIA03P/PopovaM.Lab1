6e263f176f74cd85f8616a817273e24c3a38df14
DESKTOP-6IOI2I3\Елена Третьякова <popovam03@gmail.com>
2021-12-20 09:07:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PopovaM.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.UserDomainName);
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            Console.WriteLine(Environment.ProcessId);
+            Console.WriteLine(Environment.ProcessorCount);
+            Console.WriteLine(Environment.Is64BitProcess);
+            Console.WriteLine(Environment.Is64BitOperatingSystem);
+        }
+    }
+}
